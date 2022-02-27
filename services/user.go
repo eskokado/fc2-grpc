5d22841c30e5a5f05cff724e1d5f8af989684198
eskokado <eskokado@gmail.com>
2022-02-27 11:11:45 -0300

    feat: stream both direction in the server

--- services/user.go
+++ services/user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log"
 	"time"
 
 	"github.com/eskokado/fc2-grpc/pb"
@@ -97,4 +98,23 @@ func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
 	}
 
 	return nil
+}
+
+func (*UserService) AddUserStremBoth(stream pb.UserService_AddUserStremBothServer) error {
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Error receiving stream from the client: %v", err)
+		}
+		err = stream.Send(&pb.UserResultStream {
+			Status: "Added",
+			User: req,
+		})
+		if err != nil {
+			log.Fatalf("Error sending stream to the client: %v", err)
+		}
+	}
 }
