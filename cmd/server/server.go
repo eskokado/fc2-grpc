dd482b8599db107ed75ff7e10e4cfe526ae2b57a
eskokado <eskokado@gmail.com>
2022-02-27 05:59:45 -0300

    Register service in server

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net"
 
+	"github.com/eskokado/fc2-grpc/pb"
+	"github.com/eskokado/fc2-grpc/services"
 	"google.golang.org/grpc"
 )
 
@@ -14,6 +16,9 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
+
+	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
+
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Could not serve: %v", err)
