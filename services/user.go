bbd7a71a68961473db91f691a15b3fc2dadc8ae7
eskokado <eskokado@gmail.com>
2022-02-27 09:05:10 -0300

    feat: method AddUserVerbose

--- services/user.go
+++ services/user.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/eskokado/fc2-grpc/pb"
 )
@@ -11,6 +12,8 @@ import (
 // 	AddUser(context.Context, *User) (*User, error)
 // 	mustEmbedUnimplementedUserServiceServer()
 // }
+// func (c *userServiceClient) AddUserVerbose(ctx context.Context, in *User, opts ...grpc.CallOption) (UserService_AddUserVerboseClient, error) {
+
 
 type UserService struct {
 	pb.UnimplementedUserServiceServer
@@ -29,4 +32,44 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name:  req.Name,
 		Email: req.Email,
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User: &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User: &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+	stream.Send(&pb.UserResultStream{
+		Status: "Use has been inserted",
+		User: &pb.User{
+			Id: "123",
+			Name: req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	time.Sleep(time.Second * 3)
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id: "123",
+			Name: req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	return nil
 }
